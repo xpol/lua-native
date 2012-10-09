3fc57185af72aa1f21b2732cf8cefeb411c7b49c
xpol <xpolife@gmail.com>
2012-10-09 21:19:46 +0800

    fixes test file path.

--- test.lua
+++ test.lua
@@ -1,12 +1,12 @@
 local nconv = require('nconv')
 
 print(nconv.native("这是一个UTF8字符串"))
-local f = io.open('bom.txt', 'r')
+local f = io.open('bom', 'r')
 local text = f:read('*a')
 f:close()
 local n = nconv.native(text)
 print(n)
 
-f = io.open('D:/Users/xpol/Desktop/nobom.txt', 'w')
+f = io.open('nobom', 'w')
 f:write(nconv.utf8(n))
 f:close()
