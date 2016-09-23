476bd2d8c7ac4d39f38bbc7b8a31cc529788d755
Xpol Wan <xpolife@gmail.com>
2016-09-23 13:51:39 +0800

    Fixes tests.

--- spec/native_spec.lua
+++ spec/native_spec.lua
@@ -1,3 +1,4 @@
+-- TO RUN THIS TEST, YOU NEED TO CHANGE YOU WIDNOWS CMD TO CODE PAGE 936. (RUN `CHCP 936` IN CMD WINDOW)
 local native = require('native')
 
 local function readline(filename)
@@ -7,11 +8,19 @@ local function readline(filename)
   return ln
 end
 
+local utf8 = readline('spec/data/utf8')
+local cp936 = readline('spec/data/cp936')
+
 describe('native', function()
-  describe('.encode() and .decode()', function()
-    it('convert between UTF-8 string and native encoding', function()
-      local utf8 = readline('spec/data/utf-8')
-      assert.are.equal(utf8, native.decode(native.encode(utf8)))
+  describe('.encode()', function()
+    it('convert from UTF-8 string to native encoding', function()
+      assert.are.equal(cp936, native.encode(utf8))
+    end)
+  end)
+
+  describe('.decode()', function()
+    it('convert to UTF-8 string from native encoding', function()
+      assert.are.equal(utf8, native.decode(cp936))
     end)
   end)
 end)
